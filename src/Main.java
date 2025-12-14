e0017df2b8499ce0447f9c16bb1ea793fe614057
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-12-14 23:05:05 +0530

    Addded: logic for the initial position of the player

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder game");
 
+        Player player = new Player();
+        System.out.println("Player's current position " + player.getCurrentPosition());
+
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private  int currentPosition;
+
+    public Player() {
+        this.currentPosition = 0;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+}
