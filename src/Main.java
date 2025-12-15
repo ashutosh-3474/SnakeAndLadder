0d203885d887314509f5732ae9356a10f40a62d4
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-12-15 14:29:09 +0530

    Added: logic to play with two players

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,49 @@ public class Main {
         System.out.println("Welcome to snake and ladder game");
 
         //instance of player class
-        Player player = new Player();
+        Player player1 = new Player();
+        Player player2 = new Player();
         //instance of dice class
         Dice dice = new Dice();
 
-
-        //print statements
-        System.out.println("Player's current position " + player.getCurrentPosition());
-
-        //loop to make move till player reached to 100
-        while (player.getCurrentPosition() < 100)
+        //loop to make move till any of the 2 player reached to 100
+        while (player1.getCurrentPosition() < 100 && player2.getCurrentPosition() < 100)
         {
-            //roll dice to get number of steps
-            int step = dice.roll();
-            System.out.println("step" + step);
+            //roll dice to get number of steps player 1
+            int step1 = dice.roll();
 
-            //make move
-            player.move(step);
+            //make move for player 1
+            player1.move(step1);
+
+            //check for win of player 1
+            if (player1.getCurrentPosition() == 100)
+            {
+                System.out.println("Player 1 won!!");
+                break;
+            }
+
+            //roll dice to get number of steps player 2
+            int step2 = dice.roll();
+
+            //make move for player 2
+            player2.move(step2);
+
+            //check for win of player 2
+            if (player2.getCurrentPosition() == 100)
+            {
+                System.out.println("Player 2 won!!");
+                break;
+            }
         }
 
-        System.out.println("Total dice roll made to reach win position is " + player.getTotalNumberOfDiceRoll());
+        //final state of the game
+        System.out.println("Player 1:");
+        System.out.println("Position: " + player1.getCurrentPosition());
+        System.out.println("Total number of Dice Rolls: " + player1.getTotalNumberOfDiceRoll());
+
+        System.out.println("Player 2:");
+        System.out.println("Position: " + player2.getCurrentPosition());
+        System.out.println("Total number of Dice Rolls: " + player2.getTotalNumberOfDiceRoll());
 
     }
 }
